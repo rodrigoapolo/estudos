e6a1881b264b15144fa7a0071e237f3cd5521d83
orodrigoapolo <rodrigo.gsantos@sptech.school>
2025-03-04 10:52:13 -0300

    class

--- c#/HerencaPolimorfismo/HerencaPolimorfismo/obj/Debug/net8.0/HerencaPolimorfismo.AssemblyInfo.cs
+++ c#/HerencaPolimorfismo/HerencaPolimorfismo/obj/Debug/net8.0/HerencaPolimorfismo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HerencaPolimorfismo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40fa868d81babde0d3aa8c32a0627f175c4c7e91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e41c64aa754adf1ced5e7ae09a106fdae5c797")]
 [assembly: System.Reflection.AssemblyProductAttribute("HerencaPolimorfismo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HerencaPolimorfismo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
